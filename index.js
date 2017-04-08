c855493a98d4d9ae2cf66c705a202f3407dc3f01
Heidi Goosen <Heidi Goosen>
2017-04-08 21:44:31 +1200

    populating to dropdowns

--- index.js
+++ index.js
@@ -141,7 +141,37 @@ app.get('/delete/:id',  function(req, res){
   })
 })
 
-app.listen(3001, function(){
+app.get('/getData',  function(req, res){
+  var MongoClient = mongodb.MongoClient
+  var url = "mongodb://localhost:27017/database"
+  //var editId = req.params.id
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      console.log("ooops there's an error retreiving data from Database: ", err)
+    } else {
+      var collection = db.collection("database")
+      var ObjectId = require('mongodb').ObjectId;
+      var editId = req.params.id;
+      var o_id = new ObjectId(editId);
+      collection.find({_id:o_id}).toArray(function(err, result){
+        if (err){
+          conosole.log("there is an error retreiving data from database: ", err)
+          res.send(err)
+        } else if (result.length){
+          res.send(JSON.stringify(result))
+          //console.log("this is result", result)
+        }
+        else{
+          console.log("no document found")
+          res.send("No documents found")
+        }
+        db.close()
+      })
+    }
+  })
+})
+
+app.listen(3000, function(){
   console.log("quoting up a storm on  .... 3000")
 })
 
--- public/client.js
+++ public/client.js
@@ -5,6 +5,7 @@ var request = require('superagent')
 var salesman
 var airType
 
+
 // data entry
 var fireplaceType
 var air
@@ -21,11 +22,14 @@ var cornerHearth
 var colour
 var colourPrice
 
+
 //data edit
 var editList
 
  $(document).ready(function(){
+    entryTypeController = 'quotation'
      $("#data-entry").hide()
+     $("#make").hide()
 
   $('#salesman').delegate('.border-salesman', 'click', function(e){
          $(".border-salesman").removeClass("selected")
@@ -39,7 +43,6 @@ var editList
               var select = $(this).addClass("selected")
               var airID = $(this).attr('id')
               air = select.text()
-              alert(air)
             })
 
       function hideShow(){
@@ -75,6 +78,76 @@ var editList
             $("#fogata").hide()
           }
 
+          function gasShow (){
+            $("#make").show()
+            $("#heat-glo" ).show()
+            $("#gazco" ).show()
+            $("#escea" ).show()
+            $("#living-flame" ).show()
+            $("#warmington" ).show()
+            $("#realfires" ).show()
+            $("#ortal" ).show()
+            $("#blank").hide()
+          }
+
+          function woodShow(){
+            $("#make").show()
+            $("#jayline").show()
+            $("#gazco").show()
+            $("#jetmaster").show()
+            $("#escea").show()
+            $("#bosca").show()
+            $("#pyroclassic").show()
+            $("#metro").show()
+            $("#firenzo").show()
+            $("#woodsman").show()
+            $("#esse").show()
+            $("#yunca").show()
+            $("#ethos").show()
+            $("#warmington").show()
+            $("#quadrafire").show()
+            $("#kent").show()
+            $("#stovax").show()
+            $("#mitsubishi").show()
+            $("#trendz").show()
+            $("#cast-iron-chiminea").show()
+            $("#fogata").show()
+            $("#blank").hide()
+          }
+
+          function electricShow (){
+            $("#make").show()
+            $("#gazco").show()
+            $("#esse").show()
+            $("#bromic").show()
+            $("#blank").hide()
+          }
+
+          function multifuelShow(){
+            $("#make").show()
+            $("#fisher").show()
+            $("#firenzo").show()
+            $("#esse").show()
+            $("#yunca").show()
+            $("#blank").hide()
+          }
+
+          function biofuelShow(){
+            $("#make").show()
+            $("#the-bio-flame").show()
+            $("#blank").hide()
+          }
+
+          function cookerShow(){
+            $("#make").show()
+            $("#fisher").show()
+            $("#firenzo").show()
+            $("#esse").show()
+            $("#yunca").show()
+            $("#blank").hide()
+          }
+
+
           $('#fireplace').delegate('.border-fireplace', 'click', function(e){
                 hideShow()
                  $(".border-fireplace").removeClass("selected")
@@ -82,64 +155,90 @@ var editList
                  var fireplaceID = $(this).attr('id')
                  fireplaceType = select.text()
                  if(fireplaceID == 'gas'){
-                   $("#heat-glo" ).show()
-                   $("#gazco" ).show()
-                   $("#escea" ).show()
-                   $("#living-flame" ).show()
-                   $("#warmington" ).show()
-                   $("#realfires" ).show()
-                   $("#ortal" ).show()
-                   $("#blank").hide()
+                   gasShow ()
                  } else if (fireplaceID == 'wood'){
-                   $("#jayline").show()
-                   $("#gazco").show()
-                   $("#jetmaster").show()
-                   $("#escea").show()
-                   $("#bosca").show()
-                   $("#pyroclassic").show()
-                   $("#metro").show()
-                   $("#firenzo").show()
-                   $("#woodsman").show()
-                   $("#esse").show()
-                   $("#yunca").show()
-                   $("#ethos").show()
-                   $("#warmington").show()
-                   $("#quadrafire").show()
-                   $("#kent").show()
-                   $("#stovax").show()
-                   $("#mitsubishi").show()
-                   $("#trendz").show()
-                   $("#cast-iron-chiminea").show()
-                   $("#fogata").show()
-                   $("#blank").hide()
+                    woodShow()
                  } else if (fireplaceID == 'electric') {
-                  $("#gazco").show()
-                  $("#esse").show()
-                  $("#bromic").show()
-                  $("#blank").hide()
+                   electricShow()
                 } else if(fireplaceID == "multi-fuel"){
-                  $("#fisher").show()
-                  $("#firenzo").show()
-                  $("#esse").show()
-                  $("#yunca").show()
-                  $("#blank").hide()
-
+                   multifuelShow ()
                 }else if (fireplaceID == 'biofuel'){
-                  $("#the-bio-flame").show()
-                  $("#blank").hide()
+                  biofuelShow ()
                 } else if (fireplaceID == 'cooker'){
-                  $("#fisher").show()
-                  $("#firenzo").show()
-                  $("#esse").show()
-                  $("#yunca").show()
-                  $("#blank").hide()
+                  cookerShow()
                 }
                })
 
+         $('#make').delegate('.border-make', 'click', function(e){
+            $(".border-make").removeClass("selected")
+            var select = $(this).addClass("selected")
+            var makeID = $(this).attr('id')
+            make = select.text()
+
+            if (entryTypeController == 'quotation'){
+              $.ajax({
+                url: "/fireplaceData",
+                success: function(result){
+                  console.log("this is result", result)
+                      getData = JSON.parse(result)
+                  console.log("this is getData", getData)
+                      for (i = 0; i < getData.length; i++) {
+                        dataReturn= getData[i]
+                        if(fireplaceType == dataReturn.fuel && air == dataReturn.type && dataReturn.make == make  )
+                          $(dataReturn).each(function(){
+                              var optionModel = $('<option />');
+                              optionModel.attr('value', this.model).text(this.model);
+                              $('#make-dropdown').append(optionModel);
+                              var optionCorner = $('<option />');
+                              optionCorner.attr('value', this.cornerHearth).text(this.cornerHearth);
+                              $('#cornerHearth-dropdown').append(optionCorner);
+
+                              var optionKw = $('<option />');
+                              optionKw.attr('value', this.kw).text(this.kw);
+                              $('#kw-dropdown').append(optionKw);
+
+                              var optionCleanAir = $('<option />');
+                              optionCleanAir.attr('value', this.cleanAir).text(this.cleanAir);
+                              $('#cleanAir-dropdown').append(optionCleanAir);
+
+                              var optionRural = $('<option />');
+                              optionRural.attr('value', this.rural).text(this.rural);
+                              $('#rural-dropdown').append(optionRural);
+
+                              var optionRuralWB = $('<option />');
+                              optionRuralWB.attr('value', this.ruralWB).text(this.ruralWB);
+                              $('#ruralWB-dropdown').append(optionRuralWB);
+
+                              var optionHearth = $('<option />');
+                              optionHearth.attr('value', this.hearth).text(this.hearth);
+                              $('#hearth-dropdown').append(optionHearth);
+
+                              var optionWallHearth = $('<option />');
+                              optionWallHearth.attr('value', this.wallHearth).text(this.wallHearth);
+                              $('#wallHearth-dropdown').append(optionWallHearth);
+
+                              var optionColour = $('<option />');
+                              optionColour.attr('value', this.colour).text(this.colour);
+                              $('#colour-dropdown').append(optionColour);
+
+                              var optionColourPrice= $('<option />');
+                              optionColourPrice.attr('value', this.colourPrice).text(this.colourPrice);
+                              $('#colourPrice-dropdown').append(optionColourPrice);
 
 
 
-//==
+                          });
+
+
+                      }
+                    }
+                  })
+
+            }
+           })
+
+
+//=====
 // ====
 
 //====
@@ -152,6 +251,7 @@ var editList
      $("#inbuilt-entry").hide()
      $("#quote-input").hide()
      $("#updated-data-submit").hide()
+     entryTypeController = 'data entry'
 
    });
 
@@ -165,7 +265,8 @@ var editList
 
    $('#data-submit').click(function(e){
      e.preventDefault()
-     make = $("#make").val()
+     //make = $("#make").val()
+     alert("make"+make)
      model = $("#input-model").val()
      kw = $("#input-kw").val()
      cleanAir = $("#input-clean-air").val()
@@ -236,59 +337,46 @@ var editList
       var editTemplate = ""+
         "<table>" +
           "<tr>"+
-            "<th>Edit / Delete</th>" +
-            "<th>Type</th>" +
-            "<th>Make</th>" +
-            "<th>Model</th>" +
-            "<th>Kw</th>" +
-            "<th>Clean Air</th>" +
-            "<th>Clean Air WB</th>" +
-            "<th>Rural</th>" +
-            "<th>Rural WB</th>" +
-            "<th>Hearth</th>" +
-            "<th>Wall Hearth</th>" +
-            "<th>Corner Hearth</th>" +
-            "<th>Model</th>" +
-            "<th>Colour</th>" +
-            "<th>Colour Price</th>" +
+            "<th class='table-header'>Edit / Delete</th>" +
+            "<th class='table-header'>Type</th>" +
+            "<th class='table-header'>Fuel</th>" +
+            "<th class='table-header'>Make</th>" +
+            "<th class='table-header'>Model</th>" +
+            "<th class='table-header'>Kw</th>" +
+            "<th class='table-header'>Clean Air</th>" +
+            "<th class='table-header'>Clean Air WB</th>" +
+            "<th class='table-header' >Rural</th>" +
+            "<th class='table-header'>Rural WB</th>" +
+            "<th class='table-header'>Hearth</th>" +
+            "<th class='table-header'>Wall Hearth</th>" +
+            "<th class='table-header'>Corner Hearth</th>" +
+            "<th class='table-header'>Colour</th>" +
+            "<th  class='table-header'>Colour Price</th>" +
           "</tr>"+
           "<tr>"+
-            "<td><button class='click-to-edit' data-id ="+editList._id+">Edit Entry</button>" +
-            "<button class='click-to-delete' data-id ="+editList._id+">Delete Entry</button>" +
+            "<td><button class='click-to-edit' data-id ="+editList._id+">Edit</button>" +
+            "<button class='click-to-delete' data-id ="+editList._id+">Delete</button>" +
             "</td>"+
-            "<td>"+editList.type+"</td>" +
-            "<td>"+editList.fuel+"</td>" +
-            "<td>"+editList.make+"</td>" +
-            "<td>"+editList.model+"</td>" +
-            "<td>"+editList.kw+"</td>" +
-            "<td>"+editList.cleanAir+"</td>" +
-            "<td>"+editList.cleanAirWB+"</td>" +
-            "<td>"+editList.rural+"</td>" +
-            "<td>"+editList.ruralWB+"</td>" +
-            "<td>"+editList.hearth+"</td>" +
-            "<td>"+editList.wallHearth+"</td>" +
-            "<td> "+editList.cornerHearth+"</td>" +
-            "<td>"+editList.colour+"</td>" +
-            "<td>"+editList.colourPrice+"</td>" +
+            "<td class='table-body'>"+editList.type+"</td>" +
+            "<td class='table-body'>"+editList.fuel+"</td>" +
+            "<td class='table-body'>"+editList.make+"</td>" +
+            "<td class='table-body'>"+editList.model+"</td>" +
+            "<td class='table-body'>"+editList.kw+"</td>" +
+            "<td class='table-body'>"+editList.cleanAir+"</td>" +
+            "<td class='table-body'>"+editList.cleanAirWB+"</td>" +
+            "<td class='table-body'>"+editList.rural+"</td>" +
+            "<td class='table-body'>"+editList.ruralWB+"</td>" +
+            "<td class='table-body'>"+editList.hearth+"</td>" +
+            "<td class='table-body'>"+editList.wallHearth+"</td>" +
+            "<td class='table-body'>"+editList.cornerHearth+"</td>" +
+            "<td class='table-body'>"+editList.colour+"</td>" +
+            "<td class='table-body'>"+editList.colourPrice+"</td>" +
           "</tr>"+
         "</table>"
       editLine.append(editTemplate, edit)
     }
 
-
-    /*
-  function displayEdit(edit){
-    var editTemplate = ""+
-      "<li>" +
-      "<button class='click-to-edit' data-id ="+editList._id+">Edit Entry</button>" +
-      "<button class='click-to-delete' data-id ="+editList._id+">Delete Entry</button>" +
-      "<p>Make: "+editList.make+"</p>" +
-      "<p>Model: "+editList.model+"</p>" +
-      "<p>type: "+editList.type+"</p>" +
-      "</li>"
-    editLine.append(editTemplate, edit)
-  }
-*/
+if(entryTypeController == 'data entry'){
   $.ajax({
     url: "/fireplaceData",
     success: function(result){
@@ -300,6 +388,7 @@ var editList
           }
         }
       })
+    }
 })
 
  $('#edit-data').delegate('.click-to-edit', 'click', function(){
@@ -331,52 +420,16 @@ var editList
      clickToDeleteId = $(this).attr('data-id')
      var removeLi = $(this).closest('li')
 
-   $.ajax({
-     url: "/delete/"+clickToDeleteId,
-     success: function(result){
-       console.log("clickToDelteID", clickToDeleteId)
-       removeLi.remove()
+     $.ajax({
+       url: "/delete/"+clickToDeleteId,
+       success: function(result){
+           removeLi.fadeOut(300, function(){
+             $(this).remove()
+           })
+          }
+       })
+   })
 
-        }
-     })
- })
-
-/*
-        $.ajax({
-          url: "/edit/" +$(this).attr(),
-          success: function(result){
-            editItem = JSON.parse(result)
-            for (i = 0; i < editItem.length; i++) {
-                 editItemNow = editItem[i]
-               }
-             }
-*/
-
-/*
-       $.ajax({
-         url: "/fireplaceData",
-         success: function(result){
-           editData = JSON.parse(result)
-           for (i = 0; i < editData.length; i++) {
-                editList = editData[i]
-                var tbody = $('<a href=/edit/'+editList._id+'/>').appendTo($("<table class='table'/>", {
-                    "cellspacing" : "0",
-                    "cellpadding" : "0",
-                    "border" : "0",
-                    "width" : "100%"
-
-                }).appendTo("body"))
-
-                $.each(editList, function(key, val) {
-                  return tbody
-                  //.append($("<tr/>"))
-                  .append($("<td/>").html(val))
-                })
-              }
-            }
-          })
-        })
-*/
 
    //====
    //==== quotation- entry
